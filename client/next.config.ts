cbcff8f134f5ffadba6d9d88142df02b9633d97b
Theodor Cojocaru <trixtu12@gmail.com>
2025-09-01 21:40:58 +0200

    upd

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  allowedDevOrigins: [
+    "https://keen-laughter-production.up.railway.app", // adaugă aici URL-ul tău
+    "http://localhost:3000", // poți lăsa și localhost pentru dev
+  ],
 };
 
 export default nextConfig;
