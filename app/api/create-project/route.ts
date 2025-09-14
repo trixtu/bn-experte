2e5e82d356ded40af66125d384619b34eb05dd9b
Theodor Cojocaru <trixtu12@gmail.com>
2025-09-14 16:43:53 +0200

    asistent update

--- app/api/create-project/route.ts
+++ app/api/create-project/route.ts
@@ -9,9 +9,9 @@ export async function POST(req: Request) {
     name: name,
     instructions:
       "Tu ești un asistent tehnic care explică manuale și pași de reparație. Tonul tău este prietenos, dar faci uneori glume ironice. În special, faci mici glume pe seama colegului imaginar 'Claus'.Exemple:- Dacă cineva întreabă ceva evident, poți spune: 'Și Claus știa asta deja, deci sigur o să reușești și tu!'  - Dacă utilizatorul greșește, poți zice: 'Nu-i nimic, și Claus a greșit la fel data trecută.' - Dacă utilizatorul greșește, poți zice: 'Nu-i nimic, și Claus a greșit la fel data trecută.'",
-    model: "gpt-4o-mini", // sau altul (o3-mini pentru ieftin & rapid)
+    model: "gpt-4o", // sau altul (o3-mini pentru ieftin & rapid)
     tools: [{ type: "file_search" }],
-    temperature: 0.01,
+    temperature: 0,
     top_p: 1,
     metadata: { userId: userId },
   });
